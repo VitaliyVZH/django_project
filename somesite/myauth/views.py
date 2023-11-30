ce49d1c02e924b9f66fe0a686b066583481c6c7e
Vitaliy Zhilyaev <jilyaev1987@yandex.ru>
2023-11-30 11:39:56 +0300

    Реализована возможность регистрации нового пользователя

--- somesite/myauth/views.py
+++ somesite/myauth/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LogoutView
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
+from django.views.generic import TemplateView, CreateView
 
 
 def log_out_user(request: HttpRequest):
@@ -16,3 +18,43 @@ class MyLogoutView(LogoutView):
     """
     # страница, куда будет перенаправлен пользователь после разлогирования
     next_page = reverse_lazy("myauth:login")
+
+
+class AboutMeView(TemplateView):
+    """
+    Класс реализует страницу с данными пользователя
+    """
+    template_name = "myauth/about-me.html"
+
+
+class UserRegisterView(CreateView):
+    """
+    Класс реализует регистрацию нового пользователя
+    """
+    template_name = "myauth/user-register.html"
+    # получаем дефолтную форму для создания/регистрации нового пользователя
+    form_class = UserCreationForm
+    success_url = reverse_lazy("myauth:about-me")
+
+    def form_valid(self, form):
+        """
+        Функция авторизует пользователя
+        """
+        # переопределяем родительский метод
+        response = super().form_valid(form)
+
+        # получаем введённые пользователем пароль и логин
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+
+        # функция authenticate() вернёт None, если данные не валидны для пароля
+        # и вернёт пользователя, если данные валидны
+        user = authenticate(
+            self.request,
+            password=password,
+            username=username,
+        )
+
+        # авторизация пользователя
+        login(self.request, user=user)
+        return response
